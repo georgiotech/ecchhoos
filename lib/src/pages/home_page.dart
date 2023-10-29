8a0afb1cf779245cc14a455874a206c958deacbd
georgiotech <gcroucamp@gmail.com>
2023-10-29 15:57:15 +0800

    chore: Remove unused code

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,10 +1,7 @@
-// stateless main page
 import 'package:flutter/material.dart';
 
 class PlaceholderPage extends StatelessWidget {
   const PlaceholderPage({Key? key}) : super(key: key);
-
-  static const routeName = '/';
 
   @override
   Widget build(BuildContext context) {
