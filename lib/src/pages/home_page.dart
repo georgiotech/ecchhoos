60cf3a32fd88519e2d2dd0559cf2b065427c2ded
georgiotech <gcroucamp@gmail.com>
2024-09-05 22:29:19 +0800

    feat: Create alternative layout for small screens

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -27,8 +27,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    ThemeData theme = Theme.of(context);
-
     return MultiBlocProvider(
         providers: [
           BlocProvider<PlaybackBloc>(create: (context) => PlaybackBloc()),
@@ -36,46 +34,65 @@ class _HomePageState extends State<HomePage> {
             create: (context) => ItemsBloc()..add(LoadItems()),
           ),
         ],
-        child: Row(children: [
-          SizedBox(
-            width: 200,
-            child: Column(
-              children: [
-                HomePageNavigation(),
-                Expanded(
-                  child: BlocBuilder<ItemsBloc, ItemsState>(builder: (ctx, state) {
-                    if (state is ItemsLoaded) {
-                      return PlaybackItemList(items: state.items, onItemSelected: onItemSelected);
-                    }
-                    return const Center(child: CircularProgressIndicator());
-                  }),
-                ),
-              ],
+        child: LayoutBuilder(builder: (context, constraints) {
+          if (constraints.maxWidth < 600) {
+            // small layout
+            if (selectedItem == null) {
+              return Column(children: [HomePageNavigation(), _buildListOfItems()]);
+            }
+            return _buildTranscriptViewer(selectedItem);
+          }
+          return Row(children: [
+            SizedBox(
+              width: 200,
+              child: Column(
+                children: [
+                  HomePageNavigation(),
+                  _buildListOfItems(),
+                ],
+              ),
             ),
-          ),
+            _buildTranscriptViewer(selectedItem)
+          ]);
+        }));
+  }
+
+  Widget _buildListOfItems() {
+    return Expanded(
+      child: BlocBuilder<ItemsBloc, ItemsState>(builder: (ctx, state) {
+        if (state is ItemsLoaded) {
+          return PlaybackItemList(items: state.items, onItemSelected: onItemSelected);
+        }
+        return const Center(child: CircularProgressIndicator());
+      }),
+    );
+  }
+
+  Widget _buildTranscriptViewer(TranscribedMediaItem? item) {
+    ThemeData theme = Theme.of(context);
+
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
           Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: selectedItem?.transcription == null
-                      ? const Center(
-                          child: Text(
-                          'Ec...ch...ho...os...',
-                        ))
-                      : TranscriptViewer(transcribedText: selectedItem!.transcription),
-                ),
-                Container(
-                  color: theme.splashColor,
-                  child: const Padding(
-                    padding: EdgeInsets.all(20),
-                    child: PlaybackControls(),
-                  ),
-                )
-              ],
-            ),
+            child: selectedItem?.transcription == null
+                ? const Center(
+                    child: Text(
+                    'Ec...ch...ho...os...',
+                  ))
+                : TranscriptViewer(transcribedText: selectedItem!.transcription),
           ),
-        ]));
+          Container(
+            color: theme.splashColor,
+            child: const Padding(
+              padding: EdgeInsets.all(20),
+              child: PlaybackControls(),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/src/pages/manage_remotes.dart
+++ lib/src/pages/manage_remotes.dart
@@ -31,92 +31,130 @@ class _State extends State<ManageRemotesPage> {
   Widget build(BuildContext context) {
     final navigationBloc = BlocProvider.of<NavigationBloc>(context);
 
-    return Row(
-      children: [
-        SizedBox(
-          width: 200,
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      navigationBloc.add(NavigateToApplicationPage(ApplicationPage.viewTranscripts));
-                    },
-                    icon: Icon(Icons.arrow_back),
-                    tooltip: 'Done',
-                  ),
-                  IconButton(
-                    onPressed: () async {
-                      final r = await widget.remotes;
-                      final newRemote = r!.createNewRemote();
-                      await r.saveToPreferences();
-                      onItemSelected(newRemote);
-                    },
-                    icon: Icon(Icons.add),
-                    tooltip: 'Add',
-                  ),
-                ],
-              ),
-              Expanded(
-                flex: 1,
-                child: Card(
-                    child: FutureBuilder(
-                  future: widget.remotes,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData && snapshot.data != null) {
-                      return ListView(
-                        children: snapshot.data!.registeredRemotes
-                            .map((repo) => ListTile(
-                                  key: Key((repo as MinioBackend).bucketName),
-                                  title: Text('minio://${(repo as MinioBackend).bucketName}'),
-                                  onTap: () {
-                                    onItemSelected(repo);
-                                    print(repo.uuid);
-                                  },
-                                ))
-                            .toList(),
-                      );
-                    }
-                    return Center(child: CircularProgressIndicator());
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth < 600) {
+        if (selectedItem == null) {
+          return _buildListAndShortcuts();
+        }
+        return Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    navigationBloc.add(NavigateToApplicationPage(ApplicationPage.viewTranscripts));
                   },
-                )),
-              ),
-            ],
-          ),
-        ),
-
-        // Right panel
-        Expanded(
-          flex: 2,
-          child: Card(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: selectedItem != null
-                  ? MinioRemoteForm(
-                      key: Key(selectedItem?.uuid ?? 'new'),
-                      remote: selectedItem as MinioBackend,
-                      onSave: (remote) async {
-                        final r = await widget.remotes;
-                        r?.updateRemote(remote);
-                        await r?.saveToPreferences();
-                        setState(() {});
-                      },
-                      onDelete: (uuid) async {
-                        final r = await widget.remotes;
-                        r?.removeRemote(uuid);
-                        await r?.saveToPreferences();
-                        setState(() {
-                          selectedItem = null;
-                        });
-                      },
-                    )
-                  : Center(child: Text('Select a remote to edit')),
+                  icon: Icon(Icons.arrow_back),
+                  tooltip: 'Done',
+                ),
+              ],
             ),
+            _buildEditRemoteForm(),
+          ],
+        );
+      }
+      return Row(
+        children: [
+          SizedBox(
+            width: 200,
+            child: _buildListAndShortcuts(),
           ),
+
+          // Right panel
+          _buildEditRemoteForm(),
+        ],
+      );
+    });
+  }
+
+  Widget _buildListAndShortcuts() {
+    final navigationBloc = BlocProvider.of<NavigationBloc>(context);
+
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () {
+                navigationBloc.add(NavigateToApplicationPage(ApplicationPage.viewTranscripts));
+              },
+              icon: Icon(Icons.arrow_back),
+              tooltip: 'Done',
+            ),
+            IconButton(
+              onPressed: () async {
+                final r = await widget.remotes;
+                if (r == null) {
+                  print('No remotes found. Not right.');
+                  return;
+                }
+                final newRemote = r.createNewRemote();
+                await r.saveToPreferences();
+                onItemSelected(newRemote);
+              },
+              icon: Icon(Icons.add),
+              tooltip: 'Add',
+            ),
+          ],
+        ),
+        Expanded(
+          flex: 1,
+          child: Card(
+              child: FutureBuilder(
+            future: widget.remotes,
+            builder: (context, snapshot) {
+              if (snapshot.hasData && snapshot.data != null) {
+                return ListView(
+                  children: snapshot.data!.registeredRemotes
+                      .map((repo) => ListTile(
+                            key: Key((repo as MinioBackend).bucketName),
+                            title: Text('minio://${(repo as MinioBackend).bucketName}'),
+                            onTap: () {
+                              onItemSelected(repo);
+                              print(repo.uuid);
+                            },
+                          ))
+                      .toList(),
+                );
+              }
+              return Center(child: CircularProgressIndicator());
+            },
+          )),
         ),
       ],
+    );
+  }
+
+  Widget _buildEditRemoteForm() {
+    return Expanded(
+      flex: 2,
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: selectedItem != null
+              ? MinioRemoteForm(
+                  key: Key(selectedItem?.uuid ?? 'new'),
+                  remote: selectedItem as MinioBackend,
+                  onSave: (remote) async {
+                    final r = await widget.remotes;
+                    r?.updateRemote(remote);
+                    await r?.saveToPreferences();
+                    setState(() {});
+                  },
+                  onDelete: (uuid) async {
+                    final r = await widget.remotes;
+                    r?.removeRemote(uuid);
+                    await r?.saveToPreferences();
+                    setState(() {
+                      selectedItem = null;
+                    });
+                  },
+                )
+              : Center(child: Text('Select a remote to edit')),
+        ),
+      ),
     );
   }
 }
